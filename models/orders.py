abe7f78478121f7b3bd2b19926ce3a174f963ab6
Stefano Binando <sbinando@gmail.com>
2021-11-03 14:57:37 +0000

    created controller.py and orders.py

---
+++ models/orders.py
@@ -0,0 +1,5 @@
+class Order():
+    def __init__(self, order_number, description, quantity):
+        self.order_number = order_number
+        self.description = description
+        self.quantity = quantity
