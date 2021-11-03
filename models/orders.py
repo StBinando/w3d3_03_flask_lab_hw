26af23f049e6ac5fed1f1b92a1c329291459ba42
Stefano Binando <sbinando@gmail.com>
2021-11-03 15:19:34 +0000

    orders.py modified new files fake_db.py and items.py created
    Co-authored-by: Almas <almasbutt88@users.noreply.github.com>

--- models/orders.py
+++ models/orders.py
@@ -1,5 +1,5 @@
 class Order():
-    def __init__(self, order_number, description, quantity):
+    def __init__(self, order_number, item, quantity):
         self.order_number = order_number
-        self.description = description
+        self.item = item
         self.quantity = quantity
